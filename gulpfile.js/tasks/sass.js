abc49e595c6368aad01eab73bdf0490f5b758581
Luuk Wuijster <luuk777w@users.noreply.github.com>
2020-04-22 21:10:39 +0200

    prefixcss

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -2,7 +2,7 @@ const { src, dest } = require('gulp');
 const gulpSass = require('gulp-sass');
 const concat = require('gulp-concat');
 const order = require('gulp-order');
-var path = require('path');
+const autoprefixer = require('gulp-autoprefixer');
 
 const sass = function (files_sass, filesSassOrder) {
     return function () {
@@ -10,6 +10,7 @@ const sass = function (files_sass, filesSassOrder) {
         return src(files_sass)
             .pipe(order(filesSassOrder, { base: './' }))
             .pipe(gulpSass().on('error', gulpSass.logError))
+            .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
             .pipe(concat('style.min.css'))
             .pipe(dest('./dist/css'));
     }
