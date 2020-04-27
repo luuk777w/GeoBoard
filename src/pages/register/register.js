7afde151bf26aff3a08cc45fe67cdef7b09654fb
matthijs110 <matthijs110@users.noreply.github.com>
2020-04-27 14:28:38 +0200

    Begin gemaakt met registreerproces

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -12,10 +12,50 @@ App.Register = (function () {
         App.Helpers.redirect("/login");
     }
 
+    const register = function () {
+        let username = $("#username").val();
+        let email = $("#email").val();
+        let password = $("#password").val();
+
+        let agreedToTerms = $("#terms").prop('checked');
+
+        if (! agreedToTerms) {
+            showError('.terms label', 'Please accept the terms of service to continu.')
+
+            return;
+        }
+
+        App.Authorize.Register(username, email, password).done((result) => {
+            //
+        }).fail((error) => {
+            console.log(error);
+            $('.validation-error').remove();
+
+            if (error.responseJSON) {
+
+                for (let m in error.responseJSON) {
+                    showError(`#${m.toLowerCase()}`, error.responseJSON[m][0]);
+                }
+            }
+        });
+    }
+
+    const showError = function (field, message) {
+        $(field).after(`<span class="validation-error">${message}</span>`);
+
+        console.log($(`input${field}`), message);
+    }
+
     return {
         init: _init,
-        goToLogin
+        goToLogin,
+        register
     }
 })();
 
+$(document).on('submit', '#registerForm', function (event) {
+    event.preventDefault();
+
+    App.Register.register();
+});
 $('#view').on('click', '[data-target="login"]', App.Register.goToLogin);
