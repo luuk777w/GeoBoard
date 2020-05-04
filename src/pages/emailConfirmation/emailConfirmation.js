e06bdf7719460a265558c01211cfd9bd6a7aad5e
matthijs110 <matthijs110@users.noreply.github.com>
2020-05-04 16:53:26 +0200

    Controleer op aanwezige email

--- src/pages/emailConfirmation/emailConfirmation.js
+++ src/pages/emailConfirmation/emailConfirmation.js
@@ -5,13 +5,17 @@ App.EmailConfirmation = (function () {
         console.log("emailConfirmation");
         App.Template.setLayout("auth_layout");
         App.Template.loadhtml("emailConfirmation");
+
+        let email = getEmailFromUrl();
+        if (email == null || email == "")
+        {
+            App.Helpers.redirect('/login');
+        }
     }
 
     const resendEmail = function () {
 
-        let queryString = window.location.search;
-        let urlParams = new URLSearchParams(queryString);
-        let email = urlParams.get('email');
+        let email = getEmailFromUrl();
         console.log(email);
 
         App.Helpers.toggleLoadingButton(`button`, true);
@@ -26,6 +30,13 @@ App.EmailConfirmation = (function () {
 
 
         console.log("hoi");
+    }
+
+    const getEmailFromUrl = function () {
+        let queryString = window.location.search;
+        let urlParams = new URLSearchParams(queryString);
+
+        return urlParams.get('email');
     }
 
     return {
