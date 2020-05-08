9d1961a451650b3ba580c2b1d3b7b226c0b8d995
Luuk Wuijster <luuk777w@users.noreply.github.com>
2020-05-08 02:17:01 +0200

    Uncommented things

--- src/js/template.ts
+++ src/js/template.ts
@@ -9,25 +9,25 @@ class Template {
     }
 
     public setLayout(layout: string) {
-        //if (typeof Templates[layout] === "function") {
-        //    this.layout = layout;
-        // } else {
-        //     throw new Error("Layout does not exist.")
-        //}
+        if (typeof Templates[layout] === "function") {
+            this.layout = layout;
+        } else {
+            throw new Error("Layout does not exist.")
+        }
     }
 
     public loadhtml(templateName: string, data: any = null) {
-        //if (typeof Templates[templateName] === "function") {
-        //Handlebars.registerPartial('body', Templates[templateName](data));
-        //document.getElementById('view').innerHTML = Templates[this.layout]();
-        //$("#view").removeClass();
-        //$("#view").addClass(`${templateName}-view`);
-        //$("#view").addClass(this.layout.replace('_', '-'));
+        if (typeof Templates[templateName] === "function") {
+            Handlebars.registerPartial('body', Templates[templateName](data));
+            document.getElementById('view').innerHTML = Templates[this.layout]();
+            $("#view").removeClass();
+            $("#view").addClass(`${templateName}-view`);
+            $("#view").addClass(this.layout.replace('_', '-'));
 
-        //this.setPageTitle(this.config.siteName, true);
-        //} else {
-        //throw new Error("Template does not exist.")
-        // }
+            this.setPageTitle(this.config.siteName, true);
+        } else {
+            throw new Error("Template does not exist.")
+        }
     }
 
     public setPageTitle(title: string, withoutPrefix: boolean = false) {
