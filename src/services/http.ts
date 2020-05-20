288ef6a5db0a7c582abdb0df6f2508fb5ba59c88
matthijs110 <matthijs110@users.noreply.github.com>
2020-05-20 22:35:57 +0200

    Config en HTTP

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,9 +1,53 @@
-import { singleton } from "tsyringe";
+import { singleton, container } from "tsyringe";
+import { Config } from "./config";
 
 // https://github.com/microsoft/tsyringe
 @singleton()
 export class HttpService {
+
+    private config: Config;
+
     constructor() {
-        console.log("Hallo");
+        console.log("Hallo HttpService");
+
+        this.config = container.resolve(Config);
     }
+
+    public async get(url: string) {
+        return await $.ajax({
+            type: "get",
+            url: `${this.config.apiUrl}${url}`,
+            contentType: "application/json"
+        });
+    }
+
+    public async post(url: string, data: any = null) {
+        return await $.ajax({
+            type: "post",
+            data: data,
+            url: `${this.config.apiUrl}${url}`,
+            contentType: "application/json"
+        });
+    }
+
+    public async put(url: string, data: any = null) {
+        return await $.ajax({
+            type: "put",
+            data: data,
+            url: `${this.config.apiUrl}${url}`,
+            contentType: "application/json"
+        });
+    }
+
+    public async delete(url: string) {
+        return await $.ajax({
+            type: "delete",
+
+            url: `${this.config.apiUrl}${url}`,
+            contentType: "application/json"
+        });
+    }
+
+    // TODO: Add AJAX actions with authorization
+
 }
