87efac6798c1ee7fb10e2b04ddb03dd6c7dfa801
matthijs110 <matthijs110@users.noreply.github.com>
2020-05-20 23:23:22 +0200

    Soort van begin van login

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { FormEvent } from 'react';
 import { AuthLayout } from '../../layouts/auth/auth';
 import { Link } from 'react-router-dom';
 
 import './login.scss';
+import { container } from 'tsyringe';
+import { AuthorizeService } from '../../services/authorize';
 
-export class Login extends React.Component {
+export class Login extends React.Component<{}, LoginState> {
+
+    private authorizeService: AuthorizeService;
 
     constructor(props: any) {
         super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+            rememberMe: false
+        }
+
+        this.authorizeService = container.resolve(AuthorizeService);
+    }
+
+    async onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        /**
+         * @Luuk
+         *
+         * De precieze wijze waarop we het beste forms kunnen submitten moet nog uitgezocht worden.
+         * Je kunt het namelijk 'controlled' en niet 'controlled' doen. Wat nou precies het verschil is weet ik nog niet, maar met de staat zoals het hieronder nu staat, werkt het nog niet.
+         * Dat komt omdat React het nu nog als readonly ziet doordat er geen onChange is. Als we dat even kunnen vermijden: graag. Maar aan de andere kant moeten we ook nog front-end validatie toepassen.
+         *
+         * Misschien was het je al opgevallen dat ik een begin gemaakt heb met de HttpService en de AuthorizeService. Ik heb het nog niet kunnen testen, dus kijk maar wat je ermee doet.
+         * Voor nu heb ik gewoon de structuur van de 'oude' versie overgenomen en meegenomen in de structuur zoals we die nu toepassen.
+         *
+         * Belangrijk: pull even de API. Doordat we nu met port 8888 werken, accepteerde de CORS deze applicatie niet. Na het pullen wel ;-)
+         *
+         * Tot slot wordt de JSON nog niet goed doorgeven naar de API. De payload is nu nog `username=iets&password=geheim&rememberMe=false`.
+         */
+
+        await this.authorizeService.login(this.state.username, this.state.password, this.state.rememberMe)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((e) => console.log(e))
+            .finally(() => {
+                /**
+                 * @Luuk
+                 *
+                 * FINALLY A finally BLOCK :D
+                 * Kan best handig zijn als je in beide gevallen (then of catch) hetzelfde moet doen.
+                 */
+            });
     }
 
     render() {
@@ -20,14 +64,14 @@ export class Login extends React.Component {
                     <div className="panel">
                         <div className="panel-header">Login to GeoBoard</div>
 
-                        <form id="loginForm">
+                        <form method="post" id="loginForm" onSubmit={(event) => this.onSubmit(event)}>
                             <div className="panel-body">
                                 <div className="form-group">
                                     <div className="input-group">
                                         <div className="input-prefix">
                                             <i className="fa fa-user"></i>
                                         </div>
-                                        <input type="text" id="username" name="username" placeholder="Username" autoFocus />
+                                        <input type="text" value={this.state.username} id="username" name="username" placeholder="Username" autoFocus />
                                     </div>
                                     <div className="validation-error" data-field="username"></div>
                                 </div>
@@ -37,13 +81,13 @@ export class Login extends React.Component {
                                         <div className="input-prefix">
                                             <i className="fa fa-lock"></i>
                                         </div>
-                                        <input type="password" id="password" name="password" placeholder="Password" />
+                                        <input type="password" value={this.state.password} id="password" name="password" placeholder="Password" />
                                     </div>
                                     <div className="validation-error" data-field="password"></div>
                                 </div>
 
                                 <div className="remember checkbox">
-                                    <input id="remember" type="checkbox" name="remember" />
+                                    <input id="remember" type="checkbox" name="remember" checked={this.state.rememberMe} />
                                     <label htmlFor="remember">Remember me</label>
                                 </div>
 
@@ -65,4 +109,10 @@ export class Login extends React.Component {
         )
     }
 
+}
+
+interface LoginState {
+    username: string;
+    password: string;
+    rememberMe: boolean;
 }
