d98cd0c8632cbf83a7c7111155156341db1784dd
matthijs110 <matthijs110@users.noreply.github.com>
2020-05-24 15:41:02 +0200

    Persist whitelist

--- src/store/index.ts
+++ src/store/index.ts
@@ -13,6 +13,7 @@ import storage from 'redux-persist/lib/storage'
 const persistConfig = {
     key: 'root',
     storage,
+    whitelist: ['system', 'sidebar', 'board']
 }
 
 const rootReducer = combineReducers({
