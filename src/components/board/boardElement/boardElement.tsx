403e4e171a22ba80f60eb39c7b46cd899a7365b1
Luuk Wuijster <luuk777w@users.noreply.github.com>
2020-07-02 03:46:54 +0200

    Nog een bugje

--- src/components/board/boardElement/boardElement.tsx
+++ src/components/board/boardElement/boardElement.tsx
@@ -73,6 +73,7 @@ class BoardElement extends React.Component<BoardElementProps, LocalBoardElementS
     showLargeImage(event: any) {
         if (this.state.showLargeImage == true) return;
         if (event.target.className.includes("large-image-container")) return;
+        if (this.props.element.imageId == "" || this.props.element.imageId == null) return;
 
         this.overflowStlyle = { overflow: "" };
 
